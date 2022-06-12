2989e14ec86f3acd6e28bc0141f21fba41f434c8
honeyMead <70470522+honeyMead@users.noreply.github.com>
2022-06-12 12:02:32 +0200

    enabled moving in air, prepared first action

--- Unity/Assets/FriendController.cs
+++ Unity/Assets/FriendController.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class FriendController : MonoBehaviour
 {
     private Transform player = null;
+    private PlayerController playerController = null;
     private bool isConnectedToPlayer = false;
 
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
+        playerController = player.GetComponent<PlayerController>();
     }
 
     void FixedUpdate()
     {
-        if (isConnectedToPlayer)
+        var doesPlayerAction = Input.GetKey(KeyCode.E)
+            || Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl);
+
+        if (isConnectedToPlayer && !doesPlayerAction)
         {
             var distanceToPlayer = Vector3.Distance(player.position, transform.position);
             var distanceToKeep = 0.65f;
@@ -28,6 +33,7 @@ public class FriendController : MonoBehaviour
 
                 transform.position = Vector3.MoveTowards(transform.position, player.position, speed);
                 // TODO try to not move too deep into other objects/ground
+                // TODO gravity
             }
         }
     }
@@ -36,9 +42,10 @@ public class FriendController : MonoBehaviour
     {
         var isPlayer = other.gameObject.CompareTag("Player");
 
-        if (isPlayer)
+        if (!isConnectedToPlayer && isPlayer)
         {
             isConnectedToPlayer = true;
+            playerController.Join(this.transform);
         }
     }
 }
--- Unity/Assets/PlayerController.cs
+++ Unity/Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,11 +11,13 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     private Vector3 moveDirection = Vector3.zero;
     private Transform playerModel;
+    private List<Transform> friends;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         playerModel = transform.Find("PlayerModel");
+        friends = new List<Transform>();
         // TODO restrict movement in 3rd dimension
         // TODO prevent rotation
         // TODO allow moving when in air
@@ -24,7 +27,11 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         Jump();
-        SubstractGravity();
+        var hadAction = PerformAction();
+        if (!hadAction)
+        {
+            SubstractGravity();
+        }
         ApplyMoves();
     }
 
@@ -40,27 +47,31 @@ public class PlayerController : MonoBehaviour
 
         if (moveRight || moveLeft)
         {
-            if (characterController.isGrounded)
+            var currentY = moveDirection.y;
+            //anim.SetTrigger("IsWalking");
+            if (moveRight)
             {
-                //anim.SetTrigger("IsWalking");
-                if (moveRight)
-                {
-                    moveDirection = transform.right;
-                    playerModel.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                else if (moveLeft)
-                {
-                    moveDirection = -transform.right;
-                    playerModel.rotation = Quaternion.Euler(0, 180, 0);
-                }
-                moveDirection *= speed;
+                moveDirection = transform.right;
+                playerModel.rotation = Quaternion.Euler(0, 0, 0);
             }
+            else if (moveLeft)
+            {
+                moveDirection = -transform.right;
+                playerModel.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            moveDirection *= speed;
+            moveDirection.y = currentY;
         }
         else
         {
             moveDirection = new Vector3(0f, moveDirection.y, 0f);
             //anim.SetTrigger("IsStanding");
         }
+    }
+
+    public void Join(Transform friend)
+    {
+        friends.Add(friend);
     }
 
     private void Jump()
@@ -78,9 +89,36 @@ public class PlayerController : MonoBehaviour
         //}
     }
 
+    private bool PerformAction()
+    {
+        if (Input.GetKey(KeyCode.E)
+            || Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl))
+        {
+            if (friends.Count == 1)
+            {
+                var headOfFriend = friends[0].transform.position; // TODO on top of fren
+                var distanceToFriend = Vector3.Distance(transform.position, headOfFriend);
+
+                if (distanceToFriend > 0.2f)
+                {
+                    moveDirection = Vector3.MoveTowards(transform.position, headOfFriend, 1f);
+                    return true; // TODO false if both fall
+                }
+                else
+                {
+                    Jump();
+                }
+            }
+        }
+        return false;
+    }
+
     private void SubstractGravity()
     {
-        moveDirection.y += Physics.gravity.y * Time.deltaTime;
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y += Physics.gravity.y * Time.deltaTime;
+        }
     }
 
     private void ApplyMoves()
